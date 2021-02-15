301ff3a0a6afada687c00dd6910bbd3097c7f446
LyertiaJS <68829927+lyertia@users.noreply.github.com>
2021-02-15 20:38:46 +0300

    Konsol log mesajları güncellendi

--- CommandHandler.js
+++ CommandHandler.js
@@ -1,8 +1,8 @@
 class CommandHandler {
     constructor(data = {}) {
-        if (!data.folder) throw new Error("No folder specified.");
+        if (!data.folder) throw new Error("[HANDLER] Klasör belirtilmemiş.");
         this.folder = data.folder;
-        if(!data.prefix) throw new Error("No prefix specified.");
+        if(!data.prefix) throw new Error("[HANDLER] Prefix belirtilmemiş.");
         if (!Array.isArray(data.prefix)) data.prefix = [data.prefix];
         data.prefix.sort((a, b) => a.length < b.length);
         this.prefix = data.prefix;
@@ -20,9 +20,9 @@ class CommandHandler {
             .forEach(nested => fs.readdirSync(folder + nested).forEach(f => files.push(nested + '/' + f)));
         const jsFiles = files.filter(f => f.endsWith('.js'));
 
-        if (files.length <= 0) throw new Error('No commands to load!');
+        if (files.length <= 0) throw new Error('[HANDLER] Yüklenecek komut yok!');
         const fileAmount = `${jsFiles.length}`;
-        console.log(`${fileAmount} tane dosya bulundu, yükleniyor\n`);
+        console.log(`[HANDLER] ${fileAmount} tane dosya bulundu, yükleniyor\n`);
 
         for (const f of jsFiles) {
             const file = require(folder + f);
@@ -31,13 +31,13 @@ class CommandHandler {
             const name = cmd.name;
             commands.set(name, cmd);
 
-            console.log(`Komut yükleniyor: '${name}'`);
+            console.log(`[HANDLER] Komut yükleniyor: '${name}'`);
             for (const alias of cmd.alias) {
                 aliases.set(alias, name);
             }
         }
 
-        console.log('Bütün komutlar yüklendi');
+        console.log('[HANDLER] Bütün komutlar yüklendi');
         this.commands = commands;
         this.aliases = aliases;
     }
